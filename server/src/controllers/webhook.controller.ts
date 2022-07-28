08a5a98fbe213f1153beb39d1d4413d467065a74
Amir Ahangari <amir@ahangari.org>
2022-07-28 15:06:34 -0400

    fix a minor bug

--- server/src/controllers/webhook.controller.ts
+++ server/src/controllers/webhook.controller.ts
@@ -290,15 +290,15 @@ class WebhookController {
             await mailchimpService.list(audienceId).tags.removeMembers(segment.id, [tribeMember.email]);
           }
           const spaces = await tribeClient.spaceMembers.listSpaces({ memberId: object.memberId, limit: 10 }, 'basic');
-          for (let spaceMember of spaces.nodes) {
-            const spaceId = spaceMember.space.id
+          spaces.nodes.forEach(async node => {
+            const spaceId = node.space.id
             segment = await this.getSegment(mailchimpService, { networkId, spaceId, audienceId });
             if (!segment) {
-              segment = await mailchimpService.list(audienceId).tags.create({ name: segmentPrefix + ' ' + spaceMember.space.name });
+              segment = await mailchimpService.list(audienceId).tags.create({ name: segmentPrefix + ' ' + node.space.name });
               await this.createSegmentIfNotExist({ networkId, spaceId, segmentId: segment.id });
             }
             await mailchimpService.list(audienceId).tags.addMembers(segment.id, [tribeMember.email]);
-          }
+          })
           break;
       }
       if (mailchimpConnection.sendEvents) {
