0a42968f77c1423162203471451b9f9747b75560
Amir Ahangari <amir@ahangari.org>
2022-08-19 12:34:25 -0400

    add dd-tracer

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import validateEnv from '@utils/validateEnv';
 config({ path: `.env` });
 validateEnv();
 
+import '@utils/tracer'
 import App from '@/app';
 import HealthRoute from '@routes/health.route';
 import WebhookRoute from '@routes/webhook.route';
