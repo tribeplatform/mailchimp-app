b23e40ba19fe796348a7db8a465a5ff770045b05
SaidFatah <said_designer@outlook.com>
2022-09-21 18:06:28 +0100

    refactored interaction integration

---
+++ server/src/enums/interactionTypes.enum.ts
@@ -0,0 +1,8 @@
+export enum AppInteractionType {
+  Close = 'CLOSE',
+  Reload = 'RELOAD',
+  Show = 'SHOW',
+  OpenModal = 'OPEN_MODAL',
+  OpenToast = 'OPEN_TOAST',
+  Redirect = 'REDIRECT',
+}
---
+++ server/src/enums/response.enum.ts
@@ -0,0 +1,4 @@
+export enum WebhookResponseStatus {
+  SUCCEEDED = 'SUCCEEDED',
+  FAILED = 'FAILED',
+}
