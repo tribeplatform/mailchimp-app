cbea1716a8265e9a6c5be982074af65e007b71a9
Amir Ahangari <amir@ahangari.org>
2023-05-11 16:00:30 -0400

    update UI

--- server/src/controllers/webhook.controller.ts
+++ server/src/controllers/webhook.controller.ts
@@ -130,7 +130,7 @@ const SETTINGS_BLOCK = `
             status="warning"
             title="You need to authenticate Mailchimp to activate this integration"
             />
-            <Button as="a" href="{{connectUrl}}" variant="primary">
+            <Button as="a" href="{{connectUrl}}" variant="primary" autoDisabled="false">
               Connect Mailchimp
             </Button>
         </List>
